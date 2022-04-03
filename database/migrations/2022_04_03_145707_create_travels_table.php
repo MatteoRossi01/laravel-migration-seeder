a8a734ace2368ca6afc1cb1173174b1672737daa
MatteoRossi01 <93929073+MatteoRossi01@users.noreply.github.com>
2022-04-03 17:05:57 +0200

    first commit

---
+++ database/migrations/2022_04_03_145707_create_travels_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTravelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travels', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 150);
+            $table->string('destination', 80);
+            $table->text('description')->nullable();
+            $table->unsignedTinyInteger('number_person');
+            $table->date('date_departure');
+            $table->date('date_return');
+            $table->unsignedTinyInteger('discount')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travels');
+    }
+}
